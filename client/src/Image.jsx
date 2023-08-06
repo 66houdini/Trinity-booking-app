6712c94394a4d85c0ca992df88579475ea803eb5
Tunji <oladipupoolatunji272@gmail.com>
2023-08-06 10:54:35 +0100

    minor fix

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 export default function Image({ src, ...rest }) {
+
   src = src && src.includes("https://")
     ? src
     : "http://localhost:4000/uploads/" + src;
